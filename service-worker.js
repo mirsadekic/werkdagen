6b0c8eef1e3ec3323855fda8755634274c110dcc
mirsadekic <mirsad.ekic@gmail.com>
2025-12-23 17:55:00 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "werkdagen-cache-v1";
+const CACHE_NAME = "werkdagen-cache-v3";
 const ASSETS = [
   "./",
   "./index.html",
@@ -29,6 +29,7 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then(cached => {
       if (cached) return cached;
+
       return fetch(event.request).then(resp => {
         const copy = resp.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
